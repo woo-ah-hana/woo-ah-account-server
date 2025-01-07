934621553ecf7e8f3967b65070db176a446bf44b
yoounyoungheon <iddyoon@gmail.com>
2025-01-07 14:01:46 +0900

    [feat] 거래내역 조회 dto 구현

--- src/dto.py
+++ src/dto.py
@@ -55,3 +55,43 @@ class get_account_response_dto(BaseModel):
     available_amt: int
     account_type: str
     product_name: str
+
+class get_transfers_request_dto(BaseModel):
+    bank_tran_id: str
+    fintech_use_num: str
+    inquiry_type: str
+    inquiry_base: str
+    form_date: str
+    form_time: str
+    to_date: str
+    to_time: str
+    sort_order: str
+    tran_dtime: str
+    before_inquiry_trace_info: str
+
+class transfer_res(BaseModel):
+    tran_date: str
+    tran_time: str
+    inout_type: str
+    tran_type: str
+    print_content: str
+    tran_amt: int
+    after_balance_amt: int
+    branch_name: str
+
+class get_transfers_response_dto(BaseModel):
+    api_tran_id: str
+    res_code: str
+    res_message: str
+    api_tran_dtm: str
+    bank_tran_id: str
+    bank_tran_date: str
+    bank_code_tran: str
+    bank_rsp_code: str
+    bank_rsp_message: str
+    fintech_use_num: str
+    balance_amt: str
+    page_record_cnt: str
+    next_page_yn: str
+    before_inquiry_trace_info: str
+    res_list: list[transfer_res]
